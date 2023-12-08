e27c737216e33950fc93b8cfe7c3857c933fff2e
krawnick <kraw.nick@gmail.com>
2023-12-08 19:30:56 +0500

    pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,15 +1,18 @@
 import ReactPaginate from 'react-paginate'
 import styles from './Pagination.module.scss'
+import cn from 'classnames'
 
 export const Pagination = ({ className }) => {
-  return (<ReactPaginate
-    className={styles.pagination}
-    breakLabel="..."
-    nextLabel="Следущая >"
-    onPageChange={(event) => console.log(event)}
-    pageRangeDisplayed={5}
-    pageCount={5}
-    previousLabel="< Предыдущая"
-    renderOnZeroPageCount={null}
-  />)
+  return (
+    <ReactPaginate
+      className={cn(styles.pagination, className)}
+      breakLabel="..."
+      nextLabel="Следущая >"
+      onPageChange={(event) => console.log(event)}
+      pageRangeDisplayed={5}
+      pageCount={5}
+      previousLabel="< Предыдущая"
+      renderOnZeroPageCount={null}
+    />
+  )
 }
