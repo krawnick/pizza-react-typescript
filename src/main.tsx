3c35ebc6365e8e9cd72d8e3bb1a17367824f0d38
krawnick <106428254+krawnick@users.noreply.github.com>
2023-12-13 07:47:13 +0000

    filterSlice

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </BrowserRouter>
---
+++ src/redux/filterSlice/filterSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  categoryId: 0,
+  sort: {
+    name: 'популярности',
+    sortProperty: 'rating',
+  },
+
+}
+
+export const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setCategoryId(state, action) {
+      state.categoryId = action.payload
+    },
+})
