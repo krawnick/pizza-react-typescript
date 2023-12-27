47667ed84a9fd1cd421ebd404363d571b0460c50
krawnick <kraw.nick@gmail.com>
2023-12-27 19:08:49 +0500

    button edit

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -7,8 +7,13 @@ export const Button = ({ children, className, theme, ...props }) => {
       className={cn(styles.button, className, {
         [styles.buttonOrange]: theme === 'orange',
         [styles.buttonBlack]: theme === 'black',
+
         [styles.buttonOutlineOrange]: theme === 'outline-orange',
         [styles.buttonOutlineGray]: theme === 'outline-gray',
+
+        [styles.buttonOutlineAction]: theme.includes('outline-action'),
+        [styles.buttonOutlineActionGray]: theme === 'outline-action-gray',
+        [styles.buttonOutlineActionOrange]: theme === 'outline-action-orange',
       })}
       {...props}
     >
